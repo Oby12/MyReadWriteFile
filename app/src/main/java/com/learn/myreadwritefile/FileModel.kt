38c775748b6dd9c4a61b08a76cb946bb1077792e
Bobby  Saputra <bobbysaputra514@gmail.com>
2023-09-28 17:33:28 +0700

    Initial commit

---
+++ app/src/main/java/com/learn/myreadwritefile/FileModel.kt
@@ -0,0 +1,7 @@
+package com.learn.myreadwritefile
+
+//data class filemodel di khususkan untuk menampung nama dan data dari masukan
+data class FileModel(
+    var filename : String? = null,
+    var data : String? = null
+)
